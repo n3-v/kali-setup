ed95dab7679a0eec413d419897f1aa9c48ef77fe
0xNev <nevsouth03@gmail.com>
2024-05-07 20:44:41 -0400

    .

--- setup.sh
+++ setup.sh
@@ -5,16 +5,50 @@
 sudo bash -c "echo '$(whoami) ALL=(ALL:ALL) NOPASSWD: ALL'  >>  /etc/sudoers"
 
 
-sudo apt update && sudo apt upgrade 
+sudo apt update && sudo apt upgrade
 sudo apt install --fix-missing
 
 sudo apt install i3 rofi feh xclip -y
 sudo apt install --no-install-recommends install sddm -y
 
 
-sudo apt install zsh vim tmux git -y 
+sudo apt install alcritty zsh vim tmux git -y 
 
 
 
-mkdir .vpns
 
+mkdir -p ~/.vpns
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/rofi
+
+
+
+
+echo "Setting configs"
+cp configs/.tmux.conf ~/
+cp configs/.config/i3/config ~/.config/i3/
+
+
+
+
+tmux source ~/.tmux.conf
+
+
+read -p "Reboot now (Y/n)" answer
+
+if [ -z "$answer" ]; then
+    answer="yes"
+fi
+
+case $answer in
+  [yY] | [yY][eE][sS])
+    echo "Rebooting system now..."
+    sudo shutdown -r now
+    ;;
+  [nN] | [nN][oO])
+    echo "System reboot cancelled."
+    ;;
+  *)
+    echo ""
+    ;;
+esac
