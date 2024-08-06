fbd86f46ccd16abdce5ec27782b4c8580d042f18
Nev <65242433+n3-v@users.noreply.github.com>
2024-08-06 16:35:25 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,11 +8,10 @@ sudo bash -c "echo '$(whoami) ALL=(ALL:ALL) NOPASSWD: ALL'  >>  /etc/sudoers"
 sudo apt update && sudo apt upgrade
 sudo apt install --fix-missing
 
-sudo apt install i3 rofi feh xclip -y
-sudo apt install --no-install-recommends install sddm -y
+sudo apt install i3 rofi feh xclip picom -y
 
 
-sudo apt install alcritty zsh vim tmux git -y 
+sudo apt install alcritty zsh vim tmux -y 
 
 
 
@@ -27,11 +26,13 @@ mkdir -p ~/.config/rofi
 echo "Setting configs"
 cp configs/.tmux.conf ~/
 cp configs/.config/i3/config ~/.config/i3/
+cp configs/.fegbg ~/.fegbg
 
 
 
 
 tmux source ~/.tmux.conf
+
 
 
 read -p "Reboot now (Y/n)" answer
