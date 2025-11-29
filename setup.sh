6a1bd79200356cdc4c2f30efc326bb0fa6fa48f6
Nev <65242433+n3-v@users.noreply.github.com>
2025-11-28 21:36:29 -0500

    get

--- setup.sh
+++ setup.sh
@@ -20,10 +20,12 @@ mkdir -p ~/.vpns
 cd configs && cp -r . ~/
 cd -
 
+cp wallpaper.jpg /opt/wallpaper
+
 # tmux setup
 tmux source ~/.tmux.conf
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 export PATH="$HOME/.cargo/bin:$PATH"
 . "$HOME/.cargo/env"
 
